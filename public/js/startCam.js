9f15505bbdce432a6a3d9f2181b3c170c658dd68
Bluemorpho61 <vanda10.dava@gmail.com>
2023-10-19 08:49:10 +0700

    - Penambahan Controller Baru:   (HomeController) - Penambahan laman landing page untuk melakukan presensi (wajah & RFID)

---
+++ public/js/startCam.js
@@ -0,0 +1,39 @@
+// function openCam(){
+//     let All_mediaDevices=navigator.mediaDevices
+//     if (!All_mediaDevices || !All_mediaDevices.getUserMedia) {
+//         console.log("getUserMedia() not supported.");
+//         return;
+//     }
+//     All_mediaDevices.getUserMedia({
+//         audio: true,
+//         video: true
+//     })
+//         .then(function(vidStream) {
+//             var video = document.getElementById('videoCam');
+//             if ("srcObject" in video) {
+//                 video.srcObject = vidStream;
+//             } else {
+//                 video.src = window.URL.createObjectURL(vidStream);
+//             }
+//             video.onloadedmetadata = function(e) {
+//                 video.play();
+//             };
+//         })
+//         .catch(function(e) {
+//             console.log(e.name + ": " + e.message);
+//         });
+// }
+
+
+
+var video =document.querySelector('#sensor_cam');
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+if (navigator.getUserMedia) {
+    navigator.getUserMedia({video:true},handleVideo,videoError);
+}
+function handleVideo(stream) {
+video.srcObject=stream
+}
+
+
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('mainpage.facerec');
 });
 
 Route::get('/dashboard', function () {
